49aae44571b9bf57a257a7bbe3501623aaf1d57d
RAYNingTime <ivan0kosyakov@gmail.com>
2022-05-30 11:17:55 +0300

    108. Refactoring category code part 1

--- admin/categories.php
+++ admin/categories.php
@@ -16,24 +16,8 @@
                             <small>Author</small>
                         </h1>
 							<div class="col-xs-6">
-							 <?php
-							
-							 if (isset($_POST['submit'])) {
-								$cat_title = $_POST['cat_title'];
-
-								if ($cat_title == "" || empty($cat_title)){
-									echo "This field should not be empty!";
-								} else {
-									$query = "INSERT INTO categories(cat_title) VALUES ('{$cat_title}')";
-
-									$create_category = mysqli_query($connect , $query);
-
-									if (!$create_category)
-										die('QUERY FAILED'. mysqli_error($connect));
-								}
-							 }
-
-							 ?>
+								
+							 <?php insert_categories();?>
 								
 								<form action="" method = "post">
 									<div class="form-group">
