e269dcdee070e2b47a43b35c9fe140f72d14adc8
RAYNingTime <ivan0kosyakov@gmail.com>
2022-05-30 12:56:40 +0300

    110. Creating a HTML table in admin to display a list of posts

---
+++ admin/posts.php
@@ -0,0 +1,62 @@
+<?php include "includes/admin_header.php";?>
+
+    <div id="wrapper">
+
+        <?php include "includes/admin_navigation.php";?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                    
+						  	<h1 class="page-header">
+                            Welcome to admin
+                            <small>Author</small>
+                     </h1>
+
+							<table class = "table table-bordered table-hover">
+								<thead>
+									<tr>
+										<th>ID</th>
+										<th>Category</th>
+										<th>Title</th>
+										<th>Author</th>
+										<th>Date</th>
+										<th>Image</th>
+										<th>Content</th>
+										<th>Tags</th>
+										<th>Comments</th>
+										<th>Status</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>PLACEHOLDER</td>
+										<td>PLACEHOLDER</td>
+										<td>PLACEHOLDER</td>
+										<td>PLACEHOLDER</td>
+										<td>PLACEHOLDER</td>
+										<td>PLACEHOLDER</td>
+										<td>PLACEHOLDER</td>
+										<td>PLACEHOLDER</td>
+										<td>PLACEHOLDER</td>
+										<td>PLACEHOLDER</td>
+									</tr>
+								</tbody>
+							</table>
+
+
+                     </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+<?php include "includes/admin_footer.php";?>
