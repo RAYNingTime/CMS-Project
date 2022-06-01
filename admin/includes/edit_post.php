be9cea1c13d75932221f146f84b02b99cfdc8226
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-01 14:27:35 +0300

    119. Finally Updating Posts

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -3,41 +3,90 @@ if(isset($_GET['p_id'])) {
 
 	$the_post_id = $_GET['p_id'];
 	
-	$query = "SELECT * FROM posts";
+	$query = "SELECT * FROM posts WHERE post_id = $the_post_id";
 	$select_posts_by_id = mysqli_query($connect, $query);
 
-	while($row = mysqli_fetch_assoc($select_posts_by_id)) {
-		$post_id = $row['post_id'];
-		$post_category_id = $row['post_category_id']; 
-		$post_title = $row['post_title']; 
-		$post_author = $row['post_author']; 
-		$post_date = $row['post_date']; 
-		$post_image = $row['post_image']; 
-		$post_content = $row['post_content']; 
-		$post_tags = $row['post_tags']; 
-		$post_comment_count = $row['post_comment_count']; 
-		$post_status = $row['post_status']; 
+	$row = mysqli_fetch_assoc($select_posts_by_id);
+	
+	$post_id = $row['post_id'];
+	$post_category_id = $row['post_category_id']; 
+	$post_title = $row['post_title']; 
+	$post_author = $row['post_author']; 
+	$post_date = $row['post_date']; 
+	$post_image = $row['post_image']; 
+	$post_content = $row['post_content']; 
+	$post_tags = $row['post_tags']; 
+	$post_comment_count = $row['post_comment_count']; 
+	$post_status = $row['post_status']; 
+
+}
+
+if(isset($_POST['update_post'])) {
+	 
+	$post_author = $_POST['post_author'];
+	$post_title = $_POST['post_title'];
+	$post_category_id = $_POST['post_category'];
+	$post_status = $_POST['post_status'];
+	$post_image = $_FILES['image']['name'];
+	$post_image_temp = $_FILES['image']['tmp_name'];
+	$post_content = $_POST['post_content'];
+	$post_tags = $_POST['post_tags'];
+
+	move_uploaded_file($post_image_temp, "../images/$post_image");
+
+	if(empty($post_image)){
+		$query = "SELECT * FROM posts WHERE post_id = $the_post_id";
+		$select_image = mysqli_query($connect, $query);
+
+		while($row = mysqli_fetch_assoc($select_image)){
+			$post_image = $row['post_image'];
+		}
+
 	}
+
+	$query = "UPDATE posts SET ";
+	$query .="post_title   = '{$post_title}', ";
+	$query .="post_category_id = '{$post_category_id}', ";
+	$query .="post_date    = now(), ";
+	$query .="post_author  = '{$post_author}', ";
+	$query .="post_status  = '{$post_status}', ";
+	$query .="post_tags    = '{$post_tags}', ";
+	$query .="post_content = '{$post_content}',";
+	$query .="post_image   = '{$post_image}' ";
+	$query .="WHERE post_id = {$the_post_id} ";
+
+	$update_post = mysqli_query($connect, $query);
+
+	if(!$update_post){
+		die("QUERY FAILED" . mysqli_error($connect));
+	}
+
 }
 ?>
+
+
+
+
+
+
+
+
 
 <form action="" method="post" enctype="multipart/form-data">
 
 	<div class="form-group">
 		<label for="title">Post Title</label>
-		<input value=<?php echo "'{$post_title}'"?> type="text" class="form-control" name="title">
+		<input value=<?php echo "'{$post_title}'"?> type="text" class="form-control" name="post_title">
 	</div>
 
 	<div class="form-group">
-		<select name="" id="">
+		<select name="post_category" id="">
 			<?php
-				$cat_id = $_GET['edit'];
-
 				$query = "SELECT * FROM categories";
 				$select_categories = mysqli_query($connect, $query);
 
 				if(!$select_categories){
-					die("QUERY FAILED " . mysqli_error($connection));
+					die("QUERY FAILED " . mysqli_error($connect));
 				}
 
 				while($row = mysqli_fetch_assoc($select_categories)) {
@@ -52,7 +101,7 @@ if(isset($_GET['p_id'])) {
 
 	<div class="form-group">
 		<label for="title">Post Author</label>
-		<input value=<?php echo "'{$post_author}'"?> type="text" class="form-control" name="author">
+		<input value=<?php echo "'{$post_author}'"?> type="text" class="form-control" name="post_author">
 	</div>
 
 	<div class="form-group">
@@ -78,7 +127,7 @@ if(isset($_GET['p_id'])) {
 	</div>
 
 	<div class="form-group">
-		<input type="submit" class="btn btn-primary" name="create_post" value="Publish Post">
+		<input type="submit" class="btn btn-primary" name="update_post" value="Update Post">
 	</div>
 
 </form>
