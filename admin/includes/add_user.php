ee444e40c06347de916cb5d18e8558ab2559ae0b
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-02 14:55:05 +0300

    145. Adding users in admin part 2

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -1,26 +1,26 @@
 <?php
-	if (isset($_POST['create_post'])) {
-		$post_title = $_POST['title'];
-		$post_author = $_POST['author'];
-		$post_category_id = $_POST['post_category'];
-		$post_status = $_POST['post_status'];
+	if (isset($_POST['create_user'])) {
+		$user_firstname = $_POST['user_fistname'];
+		$user_lastname = $_POST['user_lastname'];
+		$user_role = $_POST['user_role'];
 
-		$post_image = $_FILES['image']['name'];
-		$post_image_temp = $_FILES['image']['tmp_name'];
+		// $post_image = $_FILES['image']['name'];
+		// $post_image_temp = $_FILES['image']['tmp_name'];
 
-		$post_tags = $_POST['post_tags'];
-		$post_content = $_POST['post_content'];
-		$post_date = date('d-m-y');
+		$username = $_POST['username'];
+		$user_email = $_POST['user_email'];
+		$user_password = $_POST['user_password'];
+		// $post_date = date('d-m-y');
 
-		move_uploaded_file($post_image_temp, "../images/$post_image");
+		// move_uploaded_file($post_image_temp, "../images/$post_image");
 
-		$query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_status)";
-		$query .= "VALUES({$post_category_id},'{$post_title}','{$post_author}', now() ,'{$post_image}','{$post_content}','{$post_tags}','{$post_status}')";
+		// $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_status)";
+		// $query .= "VALUES({$post_category_id},'{$post_title}','{$post_author}', now() ,'{$post_image}','{$post_content}','{$post_tags}','{$post_status}')";
 
-		$create_post_query = mysqli_query($connect, $query);
+		// $create_post_query = mysqli_query($connect, $query);
 
-		if(!$create_post_query)
-			die("QUERY FAILED   " . mysqli_error($connect));
+		// if(!$create_post_query)
+		// 	die("QUERY FAILED   " . mysqli_error($connect));
 	}
 ?>
 
@@ -39,21 +39,10 @@
 	<div class="form-group">
 	<label for="title">Post Category</label>
 		<select name="user_role" id="">
-			<?php
-				$query = "SELECT * FROM users";
-				$select_users = mysqli_query($connect, $query);
+			<option value="subscriber">Select Option</option>
+			<option value="admin">Admin</option>
+			<option value="subscriber">Subscriber</option>
 
-				if(!$select_users){
-					die("QUERY FAILED " . mysqli_error($connect));
-				}
-
-				while($row = mysqli_fetch_assoc($select_users)) {
-					$user_id = $row['user_id'];
-					$user_role = $row['user_role'];
-					
-					echo "<option value='{$user_id}'>$user_role</option>";
-				}
-			?>
 		</select>
 	</div>
 
