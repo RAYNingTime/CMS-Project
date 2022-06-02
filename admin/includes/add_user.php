303a64cd493badc981fe2c9004f6e06488094a6b
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-02 15:01:31 +0300

    146. Adding users in admin part 3

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -1,6 +1,6 @@
 <?php
 	if (isset($_POST['create_user'])) {
-		$user_firstname = $_POST['user_fistname'];
+		$user_firstname = $_POST['user_firstname'];
 		$user_lastname = $_POST['user_lastname'];
 		$user_role = $_POST['user_role'];
 
@@ -14,13 +14,13 @@
 
 		// move_uploaded_file($post_image_temp, "../images/$post_image");
 
-		// $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_status)";
-		// $query .= "VALUES({$post_category_id},'{$post_title}','{$post_author}', now() ,'{$post_image}','{$post_content}','{$post_tags}','{$post_status}')";
+		$query = "INSERT INTO users(user_firstname, user_secondname, user_role, username, user_email, user_password)";
+		$query .= "VALUES('{$user_firstname}','{$user_secondname}','{$user_role}','{$username}','{$user_email}','{$user_password}')";
 
-		// $create_post_query = mysqli_query($connect, $query);
+		$create_user_query = mysqli_query($connect, $query);
 
-		// if(!$create_post_query)
-		// 	die("QUERY FAILED   " . mysqli_error($connect));
+		if(!$create_user_query)
+			die("QUERY FAILED   " . mysqli_error($connect));
 	}
 ?>
 
@@ -65,7 +65,7 @@
 
 	<div class="form-group">
 		<label for="title">Password</label>
-		<input type="password" class="form-control" name="password">
+		<input type="password" class="form-control" name="user_password">
 	</div>
 
 	<div class="form-group">
