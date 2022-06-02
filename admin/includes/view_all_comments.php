0b5276407a91e2c2a6bfbc40a007ee9436cbe302
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-02 11:50:11 +0300

    Stop 133. Deleting comments

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -57,7 +57,7 @@
 				echo "<td>{$comment_date}</td>";
 				echo "<td><a href='posts.php?source=edit_post&p_id='>Approve</a></td>";
 				echo "<td><a href='posts.php?delete='>Unapprove</a></td>";
-				echo "<td><a href='posts.php?delete='>Delete</a></td>";
+				echo "<td><a href='comments.php?delete=$comment_id'>Delete</a></td>";
 				echo "</tr>";
 			}
 			?>
@@ -68,8 +68,9 @@
 <?php 
 
 if(isset($_GET['delete'])) {
-	$get_post_id = $_GET['delete'];
-	$query = "DELETE FROM posts WHERE post_id = {$get_post_id}";
+	$get_comment_id = $_GET['delete'];
+	$query = "DELETE FROM comments WHERE comment_id = {$get_comment_id}";
 	$delete_query = mysqli_query($connect, $query);
+	header("Location: comments.php");
 }
 ?>
