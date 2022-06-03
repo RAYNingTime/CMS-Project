c3417ea654b386ad857291b27c1d40296a96f044
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-03 12:45:03 +0300

    154. Creating the login form

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,7 +2,7 @@
                 <!-- Blog Search Well -->
                 <div class="well">
                     <h4>Blog Search</h4>
-						  <form action="search.php" method ="post">
+					<form action="search.php" method ="post">
                     <div class="input-group">
                         <input name ="search" type="text" class="form-control">
                         <span class="input-group-btn">
@@ -15,7 +15,22 @@
                     <!-- /.input-group -->
                 </div>
 
-
+                <!-- Blog Search Well -->
+                <div class="well">
+                    <h4>Login</h4>
+					<form action="includes/login.php" method ="post">
+                    <div class="form-group">
+                        <input name ="username" type="text" class="form-control" placeholder="Enter username">
+                    </div>
+                    <div class="input-group">
+                        <input name ="password" type="password" class="form-control" placeholder="Enter password">
+                        <span class ="input-group-btn">
+                            <button class="btn btn-primary" name="login" type="submit">Submit</button>
+                        </span>
+                    </div>
+					</form><!-- search form -->
+                    <!-- /.input-group -->
+                </div>
 
                 <!-- Blog Categories Well -->
                 <div class="well">
