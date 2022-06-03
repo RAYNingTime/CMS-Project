6cec4328ad02d72581135c8c65419ecf92b5e971
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-03 13:30:56 +0300

    161. Login improved- Edwin from the Future

--- includes/login.php
+++ includes/login.php
@@ -25,16 +25,16 @@
 			$db_user_password = $row['user_password'];
 		}
 
-		if ($username !== $db_username || $password !== $db_user_password){
-			header("Location: ../index.php");
-		} 
-		else if ($username == $db_username && $password == $db_user_password){
+		if ($username === $db_username && $password === $db_user_password){
 			$_SESSION['username'] = $db_username;
 			$_SESSION['first_name'] = $db_user_firstname;
 			$_SESSION['last_name'] = $db_user_lastname;
 			$_SESSION['user_role'] = $db_user_role;
 			
 			header("Location: ../admin");
+		} 
+		else header("Location: ../index.php");
+
 		}
 	}
 ?>
