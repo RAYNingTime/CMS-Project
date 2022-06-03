960b5c6f8c1c6927c88963ed8bdc8137570b1b59
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-03 12:55:42 +0300

    156. Select User Query

--- includes/login.php
+++ includes/login.php
@@ -2,9 +2,21 @@
 
 <?php
 	if(isset($_POST['login'])){
-		echo $username = $_POST['username'];
-		echo $password = $_POST['password'];
+		$username = $_POST['username'];
+		$password = $_POST['password'];
 
+		$username = mysqli_real_escape_string($connect, $username);
+		$password = mysqli_real_escape_string($connect, $password);
 
+		$query = "SELECT * FROM users WHERE username = '{$username}'";
+		$select_user_query = mysqli_query($connect, $query);
+
+		if(!$select_user_query) {
+			die("QUERY FAILED " . mysqli_error($connect));
+		}
+
+		while($row = mysqli_fetch_array($select_user_query)) {
+			echo $db_id = $row['user_id'];
+		}
 	}
 ?>
