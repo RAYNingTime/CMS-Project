811ad6971968c775bafcaaa71219527a2680c9a6
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-07 11:21:55 +0300

    179. Adding Links to Post Images and Read More

--- search.php
+++ search.php
@@ -34,7 +34,8 @@
 					do {
 					$count--;
                $row = mysqli_fetch_assoc($search_query);
-
+               
+               $post_id = $row['post_id'];
                $post_title = $row['post_title'];
                $post_author = $row['post_author'];
                $post_date = $row['post_date'];
@@ -49,17 +50,19 @@
                 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#"><?php echo $post_title;?></a>
+                    <a href="post.php?p_id=<?php echo $post_id;?>"><?php echo $post_title;?></a>
                 </h2>
                 <p class="lead">
                     by <a href="index.php"><?php echo $post_author;?></a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date;?></p>
                 <hr>
+                <a href="post.php?p_id=<?php echo $post_id;?>">
                 <img class="img-responsive" src=<?php echo "'images/". $post_image. "'";?> alt="">
+                </a>
                 <hr>
                 <p><?php echo $post_content;?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id;?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
             <?php
