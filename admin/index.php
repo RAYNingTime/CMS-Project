93b49e7fe8ae6b2aed7cd165efa60a59490d12dd
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-09 14:29:06 +0300

    222. Creating the PHP code and Queries

--- admin/index.php
+++ admin/index.php
@@ -4,6 +4,23 @@
 
         <?php include "includes/admin_navigation.php";?>
 
+        <?php
+
+        $session = session_id();
+        $time = time();
+        $time_out_in_seconds = 60;
+        $time_out = $time - $time_out_in_seconds;
+
+        $query = "SELECT * FROM users_online WHERE session = '$session'";
+        $send_query = mysqli_query($connect, $query);
+        $count = mysqli_num_rows($send_query);
+
+        if($count == NULL) {
+            mysqli_query($connect, "INSERT INTO users_online(session, time) VALUES('$session', '$time')");
+        } else
+
+        ?>
+
         <div id="page-wrapper">
 
             <div class="container-fluid">
