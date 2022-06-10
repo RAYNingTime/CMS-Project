a92104d4904c6535e31af1fd667a5ffa568286e3
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-10 12:36:32 +0300

    226. Instant Users Online count without refreshing part 2
    AFTER TWO TERRIBLE HOURS I FINALLY FIXED IT! IT WAS BROKEN!

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -1,24 +1,37 @@
 <?php
 function users_online(){
-	global $connect;
-	$session = session_id();
-	$time = time();
-	$time_out_in_seconds = 30;
-	$time_out = $time - $time_out_in_seconds;
 
-	$query = "SELECT * FROM users_online WHERE session = '$session'";
-	$send_query = mysqli_query($connect, $query);
-	$count = mysqli_num_rows($send_query);
+	if(isset($_GET['onlineusers'])) {
+		global $connect;
 
-	if($count == 0) {
-			mysqli_query($connect, "INSERT INTO users_online(session, time) VALUES('$session', '$time')");
-	} else
-			mysqli_query($connect, "UPDATE users_online SET time = '$time' WHERE session = '$session'");
+		if(!$connect){
+			session_start();
+			include("../../includes/db.php");
+		}
+		
 
-	$users_online_query = mysqli_query($connect, "SELECT * FROM users_online WHERE time > '$time_out'");
-	$count_user = mysqli_num_rows($users_online_query);
-	return $count_user;
+		$session = session_id();
+		$time = time();
+		$time_out_in_seconds = 30;
+		$time_out = $time - $time_out_in_seconds;
+
+		$query = "SELECT * FROM users_online WHERE session = '$session'";
+		$send_query = mysqli_query($connect, $query);
+		$count = mysqli_num_rows($send_query);
+
+		if($count == 0) {
+				mysqli_query($connect, "INSERT INTO users_online(session, time) VALUES('$session', '$time')");
+		} else
+				mysqli_query($connect, "UPDATE users_online SET time = '$time' WHERE session = '$session'");
+
+		$users_online_query = mysqli_query($connect, "SELECT * FROM users_online WHERE time > '$time_out'");
+		$count_user = mysqli_num_rows($users_online_query);
+		echo $count_user;
+	
+	}
 }
+
+
 
 function insertCategories(){
 	global $connect;
@@ -65,7 +78,6 @@ function deleteCategories(){
 	}
 }
 
-
-
-
 ?>
+
+<?php users_online();?>
