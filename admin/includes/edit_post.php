76197c24a74fe9bbddbb4821233e003990e28c8d
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-14 12:36:48 +0300

    240. Part 4 - Edit Post Page Update

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -11,7 +11,7 @@ if(isset($_GET['p_id'])) {
 	$post_id = $row['post_id'];
 	$post_category_id = $row['post_category_id']; 
 	$post_title = $row['post_title']; 
-	$post_author = $row['post_author']; 
+	$post_user = $row['post_user']; 
 	$post_date = $row['post_date']; 
 	$post_image = $row['post_image']; 
 	$post_content = $row['post_content']; 
@@ -23,7 +23,7 @@ if(isset($_GET['p_id'])) {
 
 if(isset($_POST['update_post'])) {
 	 
-	$post_author = $_POST['post_author'];
+	$post_user = $_POST['post_user'];
 	$post_title = $_POST['post_title'];
 	$post_category_id = $_POST['post_category'];
 	$post_status = $_POST['post_status'];
@@ -48,7 +48,7 @@ if(isset($_POST['update_post'])) {
 	$query .="post_title   = '{$post_title}', ";
 	$query .="post_category_id = '{$post_category_id}', ";
 	$query .="post_date    = now(), ";
-	$query .="post_author  = '{$post_author}', ";
+	$query .="post_user = '{$post_user}', ";
 	$query .="post_status  = '{$post_status}', ";
 	$query .="post_tags    = '{$post_tags}', ";
 	$query .="post_content = '{$post_content}',";
@@ -98,8 +98,26 @@ if(isset($_POST['update_post'])) {
 	</div>
 
 	<div class="form-group">
-		<label for="title">Post Author</label>
-		<input value=<?php echo "'{$post_author}'"?> type="text" class="form-control" name="post_author">
+	<label for="users">Users</label>
+		<select name="post_user" id="">
+			<?php
+				$query = "SELECT * FROM users";
+				$select_users = mysqli_query($connect, $query);
+
+				if(!$select_users){
+					die("QUERY FAILED " . mysqli_error($connect));
+				}
+
+				while($row = mysqli_fetch_assoc($select_users)) {
+					$username = $row['username'];
+					
+					if ($username == $post_user) 
+						echo "<option value='{$username}' selected>$username</option>";
+					else
+						echo "<option value='{$username}'>$username</option>";
+				}
+			?>
+		</select>
 	</div>
 
 	<div class="form-group">
