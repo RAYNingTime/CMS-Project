41694a721c468ce1e557e2b475efe53f5fac0ab3
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-20 12:33:55 +0300

    313. Fetching the Posts

--- post.php
+++ post.php
@@ -7,7 +7,21 @@
 <?php
 
 if(isset($_POST['liked'])){
-    echo"<h1>It works</h1>";
+    $post_id = $_POST['post_id'];
+    //Select Post
+
+    $query = "SELECT * FROM posts WHERE post_id=$post_id";
+    $postResult = mysqli_query($connect, $query);
+    $post= mysqli_fetch_array($postResult);
+    $likes = $post['likes'];
+
+    if(mysqli_num_rows($postResult) >=1){
+        echo $post['post_id'];
+    }
+
+    //Update Post
+
+    //Insert data to Likes
 }
 
 ?>
