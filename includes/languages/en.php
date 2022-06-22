3d64af8fd1802963e918f5717d963c82072018a1
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-22 13:57:57 +0300

    332. Creating Language Files

---
+++ includes/languages/en.php
@@ -0,0 +1,7 @@
+<?php
+const _REGISTER = "Register";
+const _USERNAME = "Enter Desired Username";
+const _FIRSTNAME = "Enter Firstname";
+const _SECONDNAME = "Enter Lastname";
+const _EMAIL = "Enter Your Email";
+const _PASSWORD = "Enter Your Password";
---
+++ includes/languages/ukr.php
@@ -0,0 +1,7 @@
+<?php
+const _REGISTER = "Реєстрація";
+const _USERNAME = "Введіть бажаний логін";
+const _FIRSTNAME = "Введіть ім'я";
+const _SECONDNAME = "Введіть прізвище";
+const _EMAIL = "Введіть свою електронну адресу";
+const _PASSWORD = "Введіть ваш пароль";
